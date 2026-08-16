ac3a704c28a9a44615ef470d2aa5a2389b6b93a9
ChuckTripwell <levif58@walla.co.il>
2026-08-16 19:46:40 +0000

    Update sign-kernel.sh

--- build_files/sign-kernel.sh
+++ build_files/sign-kernel.sh
@@ -86,7 +86,5 @@ chmod 0644 "$SERVICE"
 mkdir -p "${TARGET_DIR}/usr/lib/systemd/system/graphical.target.wants"
 ln -s /usr/lib/systemd/system/mok-enroll.service "${TARGET_DIR}/usr/lib/systemd/system/graphical.target.wants/mok-enroll.service"
 
-systemctl enable mok-enroll.service
-
 sbverify --cert "$SIGNING_CERT" "$VMLINUZ" >/dev/null 2>&1 || error "Verification failed."
 log "Kernel signing complete."
